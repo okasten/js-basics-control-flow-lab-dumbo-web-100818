3b5e57a1524153c8caa2572dec5479770cdff004
Olivia Kasten <odkasten@gmail.com>
2018-11-18 02:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400){
+   return 'This one is on me!';
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue > 2500){
+      return 'No can do.';
+    }
+}
+
+function ternaryCheckCity(destination){
+  if (destination === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(response){
+    case: tip === "generous" {
+      return "Thank you so much.";
+    }
+  }
+}
